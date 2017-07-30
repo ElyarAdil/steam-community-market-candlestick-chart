a4f1cafb95104bd85627fed3db9b3b41ff78ec14
Elyar Adil <Elyar.Adil@gmail.com>
2017-07-30 14:32:17 +0800

    Update background.js

--- src/background.js
+++ src/background.js
@@ -12,7 +12,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'g' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'http://steamcommunity.com/market/listings/' },
+            pageUrl: { urlContains: 'steamcommunity.com/market/listings/' },
           })
         ],
         // And shows the extension's page action.
